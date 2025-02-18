c0bf768d63e659d756fc0e6139db7645c2cb443b
Shayan Ali <121148341+ShayanAliProgrammer@users.noreply.github.com>
2025-02-18 21:34:40 +0500

    Update route.ts

--- src/app/(apis)/api/chat/route.ts
+++ src/app/(apis)/api/chat/route.ts
@@ -7,7 +7,7 @@ export async function POST(req: Request) {
     const { messages, id } = await req.json();
 
     const result = streamText({
-        model: models['gemini-1.5-flash-8b-001 (Google)'],
+        model: models['gemini-2.0-flash-lite-preview-02-05 (Google)'],
         messages,
 
         // maxTokens: 1000,
